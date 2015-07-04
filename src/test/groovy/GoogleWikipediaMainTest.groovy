6453345d33866a164fb068e5302a3cde990de31a
horii003 <horii@ca2.so-net.ne.jp>
2015-07-05 08:39:01 +0900

    Create GoogleWikipediaMainTest.groovy

---
+++ src/test/groovy/GoogleWikipediaMainTest.groovy
@@ -0,0 +1,31 @@
+import geb.spock.GebReportingSpec
+import me.pdouble.page.GoogleHomePage
+import me.pdouble.page.GoogleResultsPage
+import me.pdouble.page.WikipediaPage
+
+class GoogleWikipediaMainTest extends GebReportingSpec  {
+
+	def "first result for wikipedia search should be wikipedia"() {
+		given:
+		to GoogleHomePage
+
+		expect:
+		at GoogleHomePage
+
+		when:
+		search.field.value("wikipedia")
+
+		then:
+		waitFor { at GoogleResultsPage }
+
+		and:
+		firstResultLink.text() == "Wikipedia"
+
+		when:
+		firstResultLink.click()
+
+		then:
+		waitFor { at WikipediaPage }
+	}
+
+}
