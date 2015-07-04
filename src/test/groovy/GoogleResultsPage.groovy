5ed773ed8c0a3cc01e00e373c3dce4659c7f7ace
horii003 <horii@ca2.so-net.ne.jp>
2015-07-05 08:47:25 +0900

    Create GoogleResultsPage.groovy

---
+++ src/test/groovy/GoogleResultsPage.groovy
@@ -0,0 +1,18 @@
+import geb.Page
+import GoogleSearchModule
+
+class GoogleResultsPage extends Page {
+
+	static at = { title.endsWith "Google Search" }
+	static content = {
+		// reuse our previously defined module
+		search { module GoogleSearchModule, buttonValue: "Search" }
+
+		// content definitions can compose and build from other definitions
+		results { $("li.g") }
+		result { i -> results[i] }
+		resultLink { i -> result(i).find("a") }
+		firstResultLink { resultLink(0) }
+	}
+
+}
