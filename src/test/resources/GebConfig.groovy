a46853ef87c5d2c774f84bdacce56fea120fe28f
horii003 <horii@ca2.so-net.ne.jp>
2015-07-05 10:02:28 +0900

    Update GebConfig.groovy

--- src/test/resources/GebConfig.groovy
+++ src/test/resources/GebConfig.groovy
@@ -8,6 +8,7 @@
 import org.openqa.selenium.chrome.ChromeDriver
 import org.openqa.selenium.firefox.FirefoxDriver
 import org.openqa.selenium.phantomjs.PhantomJSDriver
+import geb.spock.GebReportingSpec
 
 waiting {
 	timeout = 2
