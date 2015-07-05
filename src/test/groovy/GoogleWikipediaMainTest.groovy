2a1f2c3a5626ec236e593ce95ccee860c5a86a46
horii003 <horii@ca2.so-net.ne.jp>
2015-07-05 10:01:29 +0900

    Update GoogleWikipediaMainTest.groovy

--- src/test/groovy/GoogleWikipediaMainTest.groovy
+++ src/test/groovy/GoogleWikipediaMainTest.groovy
@@ -2,8 +2,20 @@ import geb.spock.GebReportingSpec
 import GoogleHomePage
 import GoogleResultsPage
 import WikipediaPage
+import geb.Browser
 
 class GoogleWikipediaMainTest extends GebReportingSpec  {
+
+def setupSpec() {
+    println "Inside setupSpec()..."
+    browser.config.autoClearCookies = true
+}
+
+def cleanupSpec() {
+    println "Inside cleanupSpec()..."
+    println "Quitting browser..."
+    browser.quit()
+}
 
 	def "first result for wikipedia search should be wikipedia"() {
 		given:
