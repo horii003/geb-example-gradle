5f51ec17f61d80e3da46dc4ea3d965620096f430
horii003 <horii@ca2.so-net.ne.jp>
2015-07-05 09:06:10 +0900

    Update GebConfig.groovy

--- src/test/resources/GebConfig.groovy
+++ src/test/resources/GebConfig.groovy
@@ -13,7 +13,7 @@ waiting {
 	timeout = 2
 }
 
-environments {
+
 //def browserStackBrowser = "firefox_Win8_32"
 def browserStackBrowser = System.getProperty("geb.browserstack.browser")
 if (browserStackBrowser) {
@@ -27,7 +27,7 @@ if (browserStackBrowser) {
     }
 }
 
-
+//environments {
 	// run via “./gradlew chromeTest”
 	// See: http://code.google.com/p/selenium/wiki/ChromeDriver
 //	chrome {
@@ -41,8 +41,8 @@ if (browserStackBrowser) {
 //    phantomJs {
 //        driver = { new PhantomJSDriver() }
 //    }
-}
+//}
 
 // To run the tests with all browsers just run “./gradlew test”
 
-baseUrl = "http://gebish.org"
+//baseUrl = "http://gebish.org"
