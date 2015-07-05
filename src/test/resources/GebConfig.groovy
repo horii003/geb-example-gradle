fe678410bedaac9c7385edcc7c56942f8d1f14e8
horii003 <horii@ca2.so-net.ne.jp>
2015-07-05 09:37:46 +0900

    Update GebConfig.groovy

--- src/test/resources/GebConfig.groovy
+++ src/test/resources/GebConfig.groovy
@@ -36,23 +36,25 @@ if (browserStackBrowser) {
   def browserCaps = new Properties()
   browserCaps.load(new StringReader(browserStackBrowser.replaceAll(',','\n')))
   browserstackDriver(browserCaps)
-}  
+}  else {
 
-//environments {
+environments {
 	// run via “./gradlew chromeTest”
 	// See: http://code.google.com/p/selenium/wiki/ChromeDriver
-//	chrome {
-//		driver = { new ChromeDriver() }
-//	}
+	chrome {
+		driver = { new ChromeDriver() }
+	}
 	// run via “./gradlew firefoxTest”
 	// See: http://code.google.com/p/selenium/wiki/FirefoxDriver
-//	firefox {
-//		driver = { new FirefoxDriver() }
-//	}
-//    phantomJs {
-//        driver = { new PhantomJSDriver() }
-//    }
-//}
+	firefox {
+		driver = { new FirefoxDriver() }
+	}
+    phantomJs {
+        driver = { new PhantomJSDriver() }
+    }
+}
+
+}
 
 // To run the tests with all browsers just run “./gradlew test”
 
